04f7703d6c49dbe0609600853517cca9aac3cfcc
Marco Arnulfo <arnulfomarco7@gmail.com>
2022-11-14 15:01:34 +0100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+
+<script>
+import axios from 'axios';
+import {store} from './store.js';
+export default {
+  name: 'App',
+  data(){
+    return{
+      store
+    }
+  },
+  methods : {
+    callApi(url) {
+      axios.get(url)
+      .then (response => {
+        console.log(response);
+        this.store.characters = response.data.name
+        this.store.info = response.data.img
+      })
+    } 
+  },
+  mounted(){
+    this.callApi(this.store.API_URL)
+    console.log(this.store);
+  }
+}
+
+</script>
+
+<template>
+
+<p>
+  {{store.name}}
+</p>
+
+</template>
+
+<style lang="scss" scoped>
+
+</style>
